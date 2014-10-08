271a9a76c87e89b1c148334e430c6dc12387122b
Alexander B. Gedranovich <ales@openSUSE.site>
2014-10-08 21:27:27 +0300

    Initial commit

---
+++ code/main.R
@@ -0,0 +1,16 @@
+###############################################################################
+#
+# RStackExchange / John Horton
+# oDesk Contract #13552832
+#
+# Prototyping code
+#
+# Authors:
+# Alexander Gedranovich
+# John Horton 
+# Created: 2014-10-08
+#
+# Code style follows 'Google R Style Guide'
+# https://google-styleguide.googlecode.com/svn/trunk/Rguide.xml
+###############################################################################
+
